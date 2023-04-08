be070336a1b83322a540997c97361558a513629f
Natalia Bohdanova <65080131+Natta-Sha@users.noreply.github.com>
2023-04-08 21:45:39 +0200

    phonetics implementation

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 import "./Results.css";
 
 export default function Results(props) {
@@ -7,14 +8,19 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
               <Meaning meaning={meaning} />
             </div>
           );
-
-          // meaning.definitions[0].definition;
         })}
       </div>
     );
